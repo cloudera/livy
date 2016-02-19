4e76f1572f9166568302f74ee2c6d72191e61b59
Marcelo Vanzin <vanzin@cloudera.com>
2016-02-18 17:41:42 -0800

    Increase test coverage of client-common module.
    Some of these paths are tested indirectly in other modules, but it's better
    to have more targeted unit tests.
    
    Closes #75

--- client-common/src/test/java/com/cloudera/livy/client/common/TestClientConf.java
+++ client-common/src/test/java/com/cloudera/livy/client/common/TestClientConf.java
@@ -18,15 +18,19 @@
 
 package com.cloudera.livy.client.common;
 
+import java.util.Map;
+import java.util.Properties;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestClientConf {
 
   @Test
-  public void testClientConf() {
-    TestConf conf = new TestConf();
+  public void testTypes() {
+    TestConf conf = new TestConf(null);
 
+    assertNull(conf.get(TestConf.Entry.NULL));
     assertEquals("default", conf.get(TestConf.Entry.STRING));
     assertEquals(false, conf.getBoolean(TestConf.Entry.BOOLEAN));
     assertEquals(42, conf.getInt(TestConf.Entry.INT));
@@ -50,18 +54,84 @@ public class TestClientConf {
       // Expected.
     }
 
+    conf.set(TestConf.Entry.STRING, "aString");
+    assertEquals("aString", conf.get(TestConf.Entry.STRING));
+
     conf.set(TestConf.Entry.BOOLEAN, true);
     assertEquals(true, conf.getBoolean(TestConf.Entry.BOOLEAN));
+
+    conf.set(TestConf.Entry.LONG, 42L);
+    assertEquals(42L, conf.getLong(TestConf.Entry.LONG));
+
+    conf.set(TestConf.Entry.LONG, null);
+    assertEquals(84L, conf.getLong(TestConf.Entry.LONG));
+
+    conf.set(TestConf.Entry.TIME_NO_DEFAULT, "100");
+    assertEquals(100L, conf.getTimeAsMs(TestConf.Entry.TIME_NO_DEFAULT));
+  }
+
+  @Test
+  public void testRawProperties() {
+    Properties dflt = new Properties();
+    dflt.put("key1", "val1");
+    dflt.put("key2", "val2");
+
+    TestConf conf = new TestConf(dflt);
+    conf.set("key2", "anotherVal");
+
+    assertEquals("val1", conf.get("key1"));
+    assertEquals("anotherVal", conf.get("key2"));
+
+    conf.setIfMissing("key2", "yetAnotherVal");
+    assertEquals("anotherVal", conf.get("key2"));
+
+    conf.setIfMissing("key3", "val3");
+    assertEquals("val3", conf.get("key3"));
+
+    int count = 0;
+    for (Map.Entry<String, String> e : conf) {
+      count++;
+    }
+    assertEquals(3, count);
+
+    TestConf newProps = new TestConf(null);
+    newProps.set("key4", "val4");
+    newProps.set("key5", "val5");
+    conf.setAll(newProps);
+    assertEquals("val4", conf.get("key4"));
+    assertEquals("val5", conf.get("key5"));
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void testInvalidTime() {
+    TestConf conf = new TestConf(null);
+    conf.set(TestConf.Entry.TIME, "invalid");
+    conf.getTimeAsMs(TestConf.Entry.TIME);
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void testInvalidTimeSuffix() {
+    TestConf conf = new TestConf(null);
+    conf.set(TestConf.Entry.TIME, "100foo");
+    conf.getTimeAsMs(TestConf.Entry.TIME);
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void testTimeWithoutDefault() {
+    TestConf conf = new TestConf(null);
+    conf.getTimeAsMs(TestConf.Entry.TIME_NO_DEFAULT);
   }
 
   private static class TestConf extends ClientConf<TestConf> {
 
     static enum Entry implements ConfEntry {
+      NULL("null", null),
       STRING("string", "default"),
       BOOLEAN("bool", false),
       INT("int", 42),
       LONG("long", 84L),
-      TIME("time", "168ms");
+      TIME("time", "168ms"),
+      TIME_NO_DEFAULT("time2", null);
 
       private final String key;
       private final Object dflt;
@@ -79,8 +149,8 @@ public class TestClientConf {
 
     }
 
-    TestConf() {
-      super(null);
+    TestConf(Properties p) {
+      super(p);
     }
 
   }
