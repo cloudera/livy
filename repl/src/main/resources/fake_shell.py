03fe9aa286dab8a6d5594393149ed9ee360f906c
Jeff Zhang <zjffdu@apache.org>
2016-03-11 10:20:33 -0800

    LIVY-89. PySparkInterpreter doesn't work
    Closes #88

--- repl/src/main/resources/fake_shell.py
+++ repl/src/main/resources/fake_shell.py
@@ -24,6 +24,7 @@ import sys
 import traceback
 import StringIO
 import base64
+import os
 
 logging.basicConfig()
 LOG = logging.getLogger('fake_shell')
@@ -388,6 +389,10 @@ def main():
     sys.stderr = fake_stderr
 
     try:
+        if os.environ.get("livy.test") != "true":
+            # Load spark into the context
+            exec 'from pyspark.shell import sc' in global_dict
+
         print >> sys_stderr, fake_stdout.getvalue()
         print >> sys_stderr, fake_stderr.getvalue()
 
@@ -451,6 +456,9 @@ def main():
             print >> sys_stdout, response
             sys_stdout.flush()
     finally:
+        if os.environ.get("livy.test") != "true" and 'sc' in global_dict:
+            global_dict['sc'].stop()
+
         sys.stdin = sys_stdin
         sys.stdout = sys_stdout
         sys.stderr = sys_stderr
