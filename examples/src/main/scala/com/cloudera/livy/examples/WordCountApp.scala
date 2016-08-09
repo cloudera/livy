505260065b5db228fe9d4e73fa69955236783f4a
manikandan.nagarajan <manikandan.n2010@gmail.com>
2016-08-09 10:40:00 -0700

    Changes in docs

--- examples/src/main/scala/com/cloudera/livy/examples/WordCountApp.scala
+++ examples/src/main/scala/com/cloudera/livy/examples/WordCountApp.scala
@@ -32,23 +32,23 @@ import com.cloudera.livy.scalaapi.{LivyScalaClient, ScalaJobHandle, _}
 
 /**
  *  A WordCount example using Scala-API which reads text from a stream and saves
- *  it as data frames. The word with maximum count is the result
+ *  it as data frames. The word with maximum count is the result.
  */
 object WordCountApp {
 
   var scalaClient: LivyScalaClient = null
 
   /**
-   *  Initializes the scala client with the given url
-   *  @param url The livy server url
+   *  Initializes the Scala client with the given url.
+   *  @param url The Livy server url.
    */
   def init(url: String): Unit = {
     scalaClient = new LivyClientBuilder(false).setURI(new URI(url)).build().asScalaClient
   }
 
   /**
-   *  Uploads the scala-api Jar and the examples Jar from the target directory
-   *  @throws FileNotFoundException If either of scala-api Jar or examples Jar is not found
+   *  Uploads the Scala-API Jar and the examples Jar from the target directory.
+   *  @throws FileNotFoundException If either of Scala-API Jar or examples Jar is not found.
    */
   @throws(classOf[FileNotFoundException])
   def uploadRelevantJarsForJobExecution(): Unit = {
@@ -77,15 +77,15 @@ object WordCountApp {
   }
 
   /**
-   * Submits a spark streaming job to the livy server
+   * Submits a spark streaming job to the livy server.
    *
    * The streaming job reads data from the given host and port. The data read
    * is saved in json format as data frames in the given output path file. If the file is present
    * it appends to it, else creates a new file. For simplicity, the number of streaming batches
-   * are 2 with each batch for 20 seconds. The Timeout of the streaming job is set to 40 seconds
-   * @param host Hostname that Spark Streaming context has to connect for receiving data
-   * @param port Port that Spark Streaming context has to connect for receiving data
-   * @param outputPath Output path to save the processed data read by the Spark Streaming context
+   * are 2 with each batch for 20 seconds. The Timeout of the streaming job is set to 40 seconds.
+   * @param host Hostname that Spark Streaming context has to connect for receiving data.
+   * @param port Port that Spark Streaming context has to connect for receiving data.
+   * @param outputPath Output path to save the processed data read by the Spark Streaming context.
    */
   def processStreamingWordCount(
       host: String,
@@ -110,11 +110,11 @@ object WordCountApp {
   }
 
   /**
-   * Submits a spark sql job to the livy server
+   * Submits a spark sql job to the livy server.
    *
    * The sql context job reads data frames from the given json path and executes
-   * a sql query to get the word with max count on the temp table created with data frames
-   * @param inputPath Input path to the json data containing the words
+   * a sql query to get the word with max count on the temp table created with data frames.
+   * @param inputPath Input path to the json data containing the words.
    */
   def getWordWithMostCount(inputPath: String): ScalaJobHandle[String] = {
     scalaClient.submit { context =>
@@ -150,41 +150,41 @@ object WordCountApp {
 
   /**
    * Main method of the WordCount App. This method does the following
-   * - Validate the arguments
-   * - Initializes the scala client of livy
-   * - Uploads the required livy and app code jar files to the spark cluster needed during runtime
+   * - Validate the arguments.
+   * - Initializes the scala client of livy.
+   * - Uploads the required livy and app code jar files to the spark cluster needed during runtime.
    * - Executes the streaming job that reads text-data from socket stream, tokenizes and saves
-   *   them as dataframes in JSON format in the given output path
+   *   them as dataframes in JSON format in the given output path.
    * - Executes the sql-context job which reads the data frames from the given output path and
-   * and returns the word with max count
+   * and returns the word with max count.
    *
    * @param args
    *
    * REQUIRED ARGUMENTS
-   * arg(0) - Livy server url
-   * arg(1) - Output path to save the text read from the stream
+   * arg(0) - Livy server url.
+   * arg(1) - Output path to save the text read from the stream.
    *
    * OPTIONAL ARGUMENTS(Default values: arg(2) = 'localhost', arg(3) = 8086)
-   * arg(2) - Socket stream host that spark streaming will listen to
-   * arg(3) - Socket stream port that spark streaming will listen to
+   * arg(2) - Socket stream host that spark streaming will listen to.
+   * arg(3) - Socket stream port that spark streaming will listen to.
    * Optional arguments need to be passed in a key=value way
    * Example:
    * host=examplehost where "host" is the required key for arg(2)
    * port=8080 where "port" is the required key for arg(3)
    *
    * STEPS FOR EXECUTION - To get accurate results for one execution:
-   * 1) Delete if the file already exists in the given outputFilePath
+   * 1) Delete if the file already exists in the given outputFilePath.
    *
    * 2) Spark streaming will listen to the given or defaulted host and port. So textdata needs to be
    *    passed as socket stream during the run-time of the App. The streaming context reads 2
    *    batches of data with an interval of 20 seconds for each batch. All the data has to be
    *    fed before the streaming context completes the second batch. NOTE - Inorder to get accurate
    *    results for one execution, pass the textdata before the execution of the app so that all the
-   *    data is read by the socket stream
+   *    data is read by the socket stream.
    *    To pass data to localhost and port 8086 provide the following command
    *    nc -kl 8086
    *
-   * 3) The text can be provided as paragraphs as the app will tokenize the data and filter spaces
+   * 3) The text can be provided as paragraphs as the app will tokenize the data and filter spaces.
    *
    * 4) Execute the application jar file with the required and optional arguments either using
    * mvn or scala.
