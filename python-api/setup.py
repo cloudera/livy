c29f3fe1bfd676816a76acd63844725be7ee955e
Marcelo Vanzin <vanzin@cloudera.com>
2016-08-29 12:43:43 -0700

    Clean up python-api build, directory layout.
    - move tests into the "src/test" tree
    - remove versioneer since it seems to cause more problems than it solves
    - hardcode the Livy version into setup.py; we just need to rememeber to
      change it before release.
    - remove the deployment step; we can work on creating a "bdist" separately.

--- python-api/setup.py
+++ python-api/setup.py
@@ -19,13 +19,6 @@
 from setuptools import setup
 import os
 import io
-import versioneer
-
-readme_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                           os.pardir))
-with io.open(os.path.join(readme_path, 'README.rst'), encoding='utf-8') \
-        as long_description_file:
-    long_description = long_description_file.read()
 
 DESCRIPTION = "A simple Python API for Livy powered by requests"
 
@@ -42,23 +35,22 @@ requirements = [
     'configparser>=3.5.0',
     'future>=0.15.2',
     'futures>=3.0.5',
-    'pytest-runner>=2.9',
     'requests>=2.10.0',
     'responses>=0.5.1',
-    'twine>=1.6.5'
 ]
 
 setup(
     name='livy-python-api',
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    packages=["livy", "tests"],
-    package_dir={"": "src/main/python"},
+    version="0.3.0-SNAPSHOT",
+    packages=["livy", "livy-tests"],
+    package_dir={
+      "" : "src/main/python",
+      "livy-tests" : "src/test/python/livy-tests",
+    },
     url='https://github.com/cloudera/livy',
     author_email='livy-user@cloudera.org',
     license='Apache License, Version 2.0',
     description=DESCRIPTION,
-    long_description=long_description,
     platforms=['any'],
     keywords='livy pyspark development',
     classifiers=CLASSIFIERS,
