e741bdb5364256651a199b0dbde8f53aa0fd48b0
jerryshao <sshao@hortonworks.com>
2017-01-25 15:37:37 +0800

    Bump to 0.4.0-SNAPSHOT for master branch
    Change-Id: I32dd970bfa1150e3b47ecbfd7c1a7c348bb7e259

--- python-api/setup.py
+++ python-api/setup.py
@@ -40,7 +40,7 @@ requirements = [
 
 setup(
     name='livy-python-api',
-    version="0.3.0-SNAPSHOT",
+    version="0.4.0-SNAPSHOT",
     packages=["livy", "livy-tests"],
     package_dir={
         "": "src/main/python",
