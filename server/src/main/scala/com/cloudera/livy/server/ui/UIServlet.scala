0020e68cf83697f4a288edd63066ef477ebe1879
Alex Bozarth <ajbozart@us.ibm.com>
2017-05-01 13:17:43 -0700

    added conf to template, cleaned up missed code from last commit

--- server/src/main/scala/com/cloudera/livy/server/ui/UIServlet.scala
+++ server/src/main/scala/com/cloudera/livy/server/ui/UIServlet.scala
@@ -47,7 +47,7 @@ class UIServlet(livyConf: LivyConf) extends ScalatraServlet {
         </div>
         <div class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">{pageName}</a></li>
+            <li><a href="#">{pageName}</a></li>
           </ul>
         </div>
       </div>
